dde56abc6107f9e8d90e5a40a82693c4690f65cb
abidino <abidinozd@gmail.com>
2023-03-26 20:01:27 +0300

    initialize

---
+++ src/main/java/dev/abidino/graalvm/GraalvmApplication.java
@@ -0,0 +1,20 @@
+package dev.abidino.graalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GraalvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	String helloWorld() {
+		return "Hello World!";
+	}
+}
